616a69b79e211ac2ca58365ff6d2376a9d383002
MohamedEmbrator <embrator233@gmail.com>
2025-08-19 16:26:26 +0300

    edit db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-function connectToDB() {
-  mongoose
+async function connectToDB() {
+  await mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("Connected To DB"))
     .catch((error) => console.log("Connection Failed: ", error));
